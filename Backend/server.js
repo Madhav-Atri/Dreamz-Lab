fd114c2723fdea0b762a0f39e85cfcb556a1299b
Madhav-Atri <madhavalter@gmail.com>
2026-07-11 04:41:39 +0530

    Configured server.js with child_process to connect and execute docker commands

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const cors = require("cors");
 
+const {exec} = require("child_process")
+
 const app = express();
 
 app.use(cors());
@@ -15,4 +17,16 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
 
+app.get("/test", (req, res) => {
+    exec("docker images" , (error, stdout, stderr) => {
+        if (error){
+            console.error(error);
+            return res.status(500).send(error.message);
+        }
+        if (stderr){
+            console.log(stderr);
+        }
 
+        res.send("<pre>" + stdout + "</pre>");
+    });
+});
